6c4319f6d3164929a9e095c3edb2a3ba095b6063
Dom-Komputer <lolin515@wp.pl>
2024-12-13 11:22:35 +0100

    final

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -18,12 +18,12 @@ class TaskController extends Controller
             return [
                 'id' => $task -> id,
                 'name' => $task -> name,
-                'description' => $task -> description,
+                'piority' => $task -> piority,
                 'deadline' => $task -> deadline,
                 'links' => [
-                    'self' => route('task.show', $task -> id),
-                    'update' => route('task.update', $task -> id),
-                    'delete' => route('task.destroy', $task -> id),
+                    'self' => route('tasks.show', $task -> id),
+                    'update' => route('tasks.update', $task -> id),
+                    'delete' => route('tasks.destroy', $task -> id),
                 ]
             ];
         });
@@ -37,14 +37,48 @@ class TaskController extends Controller
      */
     public function store(Request $request)
     {
-        $newTask
+        $newTask = Task::create([
+            'projectid' => $request -> projectid,
+            'name' => $request -> name,
+            'piority' => $request -> piority,
+            'deadline' => $request -> deadline,
+        ]);
+
+        return response() -> json([
+            'message' => 'Task creating successful',
+            'data' => [
+                'projectid' => $newTask -> projectid,
+                'name' => $newTask -> name,
+                'piority' => $newTask -> piority,
+                'deadline' => $newTask -> deadline,
+            ],
+            'links' => [
+                'self' => route('tasks.show', $newTask -> id),
+                'update' => route('tasks.update', $newTask -> id),
+                'delete' => route('tasks.destroy', $newTask -> id),
+            ]
+        ], 201, [], JSON_UNESCAPED_SLASHES);  
     }
      /**
      * Display the specified resource.
      */
     public function show($id)
     {
-        //
+        $task = Task::find($id);
+        
+        return response() -> json([
+            'data' => [
+                'projectid' => $task -> projectid,
+                'name' => $task -> name,
+                'piority' => $task -> piority,
+                'deadline' => $task -> deadline,
+            ],
+            'links' => [
+                'self' => route('tasks.show', $task -> id),
+                'update' => route('tasks.update', $task -> id),
+                'delete' => route('tasks.destroy', $task -> id),
+            ]
+        ], 201, [], JSON_UNESCAPED_SLASHES);  
     }
 
     /**
@@ -52,14 +86,39 @@ class TaskController extends Controller
      */
     public function update(Request $request, int $id)
     {
-        //
+        $task = Task::find($id);
+
+        $task -> projectid = $request -> projectid;
+        $task -> name = $request -> name;
+        $task -> piority = $request -> piority;
+        $task -> deadline = $request -> deadline;
+
+        $task -> save();
+
+        return response()->json([
+            'message' => 'Task edited successful',
+            'links' => [
+                'self' => route('tasks.show', $task->id),
+                'update' => route('tasks.update', $task->id),
+                'delete' => route('tasks.destroy', $task->id),
+            ]
+        ], 200, [], JSON_UNESCAPED_SLASHES);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Task $task)
+    public function destroy($id)
     {
-        //
+        Task::where('id', '=', $id) -> delete();
+        
+        return response()->json([
+            'message' => 'Task deleted successful',
+            'links' => [
+                'create' => route('tasks.store'),
+                'list' => route('tasks.index'),
+            ]
+        ], 200, [], JSON_UNESCAPED_SLASHES); 
+
     }
 }
